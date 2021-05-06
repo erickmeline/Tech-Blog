f4a7cefd54dabdd77706d6c8e2e98376722ef7de
Erick Meline <erickmeline@gmail.com>
2021-05-06 15:18:45 -0700

    add link

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,46 +41,48 @@ router.get('/:id', (req, res) => {
 	});
 });
 
-router.post('/', (req, res) => {
-	User.create({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password,
-	}).then(response => {
-		req.session.save(() => {
-			req.session.user_id = response.id;
-			req.session.username = response.username;
-			req.session.logged_in = true;
-			// res.json(response);
-			res.status(200).json('dashboard')
-		});
-	})//.then((response) => res.status(200).json('dashboard'))
-	.catch((err) => res.status(404).json('homepage'));
+router.post("/", (req, res) => {console.log('=================== got here =============');
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  }).then((response) => {console.log('=================== got here =============');
+      req.session.save(() => {
+        req.session.user_id = response.id;
+        req.session.username = response.username;
+        req.session.logged_in = true;
+        res.json(response);
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
 	User.findOne({
 		where: {
-			email: req.body.email
-		}
-	}).then(response => {
+			email: req.body.email,
+		},
+	}).then((response) => {
 		if (!response) {
-			res.status(400).json('homepage');
+			res.status(400).json({ message: "User not found" });
 			return;
 		}
 		const validPassword = response.checkPassword(req.body.password);
 		if (!validPassword) {
-			res.status(400).json('homepage');
+			res.status(400).json({ message: "Incorrect Password!" });
 			return;
 		}
 		req.session.save(() => {
 			req.session.user_id = response.id;
 			req.session.username = response.username;
-			req.session.logged_in = true;
-			res.status(200).json('dashboard');
-			res.json({ user: response, message: 'logged in' });
+			req.session.loggedIn = true;
+			res.json({ user: response, message: "You are now logged in!" });
 		});
-	}).then((response) => res.status(200).json('dashboard'));
+	}).catch((err) => {
+		res.status(500).json(err);
+	});
 });
 
 router.post('/logout', (req, res) => {
